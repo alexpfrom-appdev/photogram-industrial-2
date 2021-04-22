64a18aab695e59ac4c1ce8f5f5a5eea8b5832e2e
alexpfrom <afrom@chicagobooth.edu>
2021-04-22 23:40:10 +0000

    quick

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  get ":username/liked" => "photos#liked", as: :liked_photos
-  get ":username/feed" => "photos#liked", as: :liked_photos
-  get ":username/followers" => "photos#liked", as: :liked_photos
-  get ":username/following" => "photos#liked", as: :liked_photos
+  # get ":username/liked" => "photos#liked", as: :liked_photos
+  # get ":username/feed" => "photos#liked", as: :liked_photos
+  # get ":username/followers" => "photos#liked", as: :liked_photos
+  # get ":username/following" => "photos#liked", as: :liked_photos
 
   get ":username" => "users#show", as: :user
 end
